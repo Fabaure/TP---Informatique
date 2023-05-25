9a24c034a34a5306e0d2e5eb3218265721e48ad2
Aurélien LITRA <aurelien.litra@gmail.com>
2023-05-25 15:37:43 +0200

    Test2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
-#include "Circle.h"
-#include "Rectangle.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include "Dessins.h"
 
+using namespace cv;
 
 int main() {
-	cout << Circle() << Rectangle();
+    int width = 800;
+    int height = 600;
+
+    Dessin dessin(width, height);
+
+    // Création d'un triangle
+    Triangle* triangle = new Triangle();
+    dessin.ajouterForme(triangle);
+
+    // Création d'une ligne droite
+    LigneDroite* ligne = new LigneDroite();
+    dessin.ajouterForme(ligne);
+
+    // Dessiner les formes
+    dessin.dessinerFormes();
+
+    // Afficher le dessin
+    dessin.afficher();
+
+    delete triangle;
+    delete ligne;
+
+    return 0;
 }
